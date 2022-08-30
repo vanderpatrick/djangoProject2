35cf9b9c16bff56b1d61d0e2ada6aa8dc5f122ba
vanderpatrick <patrick123321123@icloud.com>
2022-08-30 11:43:45 +0000

    a

---
+++ users/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required 
+from .forms import UserRegister 
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegister(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Your account has been created ! you may now login')
+        return  redirect('login')
+    else:
+        form = UserRegister ()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
